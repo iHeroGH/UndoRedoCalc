e24823658b615ce6ddcf581f2b9d70c9ff980fb0
George Matta <jojomedhat2004@gmail.com>
2021-04-06 21:50:30 -0700

    Created the main UI (text enter, operators, undo/redo)

--- app/src/main/java/georgematta/undoredocalc/MainActivity.java
+++ app/src/main/java/georgematta/undoredocalc/MainActivity.java
@@ -3,8 +3,29 @@ package georgematta.undoredocalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String operator;
+    private String secondOperand;
+
+    public void eqClick(View view){
+        return;
+    }
+
+    public void operClick(View view){
+
+        String operatorChosen = ((Button) view).getText().toString();
+
+        this.operator = operatorChosen;
+
+        Log.i("New Operator Chosen", this.operator);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
